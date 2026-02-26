f64845c10fbfa5045036bb3ae2d833e1d8e0b61f
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2026-02-26 18:24:39 +0200

    set default font

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,8 @@ import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 
+import '@fontsource-variable/geist';
+
 import appCss from "../styles.css?url";
 
 export const Route = createRootRoute({
